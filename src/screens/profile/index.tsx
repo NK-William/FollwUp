60ad81cf83f8100b08ca043c50542fb679901bae
Nkuna William <64930245+NK-William@users.noreply.github.com>
2023-09-18 22:06:19 +0200

    profile screen

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,12 +1,27 @@
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import React from 'react';
 import getStyling from './style';
 import {useProfile} from './util';
 
 const Profile = () => {
+  const styles = getStyling();
   return (
-    <View>
-      <Text>Profile screen</Text>
+    <View style={styles.container}>
+      <View style={styles.profilePicContainer}>
+        <Image
+          style={styles.profilePic}
+          source={require('../../assets/images/profilePicture.jpeg')}
+        />
+      </View>
+      <View style={styles.profileInfoCardContainer}>
+        <View style={styles.profileInfoCard}></View>
+      </View>
+      <View style={styles.backArrowContainer}>
+        <Image
+          style={styles.backArrowIcon}
+          source={require('../../assets/images/backArrow.png')}
+        />
+      </View>
     </View>
   );
 };
--- src/screens/profile/style.ts
+++ src/screens/profile/style.ts
@@ -1,7 +1,51 @@
 import {StyleSheet} from 'react-native';
 
 const getStyling = () => {
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    backArrowContainer: {
+      backgroundColor: 'white',
+      position: 'absolute',
+      height: 40,
+      width: 40,
+      borderRadius: 24,
+      justifyContent: 'center',
+      alignItems: 'center',
+      top: 12,
+      left: 12,
+    },
+    profilePicContainer: {flex: 4},
+    profileInfoCardContainer: {
+      flex: 2,
+    },
+    profilePic: {
+      width: '100%',
+      height: '100%',
+      resizeMode: 'cover',
+    },
+    backArrowIcon: {
+      height: 20,
+      width: 20,
+      resizeMode: 'contain',
+    },
+    profileInfoCard: {
+      backgroundColor: '#E6E6E6',
+      height: '105%',
+      marginHorizontal: 20,
+      marginTop: -45,
+      borderRadius: 20,
+      elevation: 5,
+      shadowColor: 'black',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.3,
+      shadowRadius: 5,
+    },
+  });
 };
 
-export default getStyling();
+export default getStyling;
