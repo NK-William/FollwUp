cda14c04aeab66b8a5899a325bbe80d6d6d74fe9
Nkuna William <64930245+NK-William@users.noreply.github.com>
2024-09-16 23:05:21 +0200

    Minor changes

--- src/components/profileButton/index.tsx
+++ src/components/profileButton/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text, Pressable, Image } from 'react-native'
 import React, { FC } from 'react'
 import getStyling from './style'
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileButton: FC = () => {
+    const navigation = useNavigation();
     const styles = getStyling()
   return (
-    <Pressable onPress={() => {}}>
+    <Pressable onPress={() => navigation.navigate('Profile')}>
             <Image
             source={require('../../assets/images/profilePlaceholder.png')}
             style={styles.profilePlaceholder}
