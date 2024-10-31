6a109be2a6e3f1fe924cad889bd55d7e4d7c9bec
Nkuna William <64930245+NK-William@users.noreply.github.com>
2024-10-31 23:47:03 +0200

    Clean-ups

--- src/screens/addTask/index.tsx
+++ src/screens/addTask/index.tsx
@@ -15,164 +15,31 @@ import {
 import {BackButton, Icon} from '../../components';
 import {accent, close} from '../../constants/colors';
 import getGlobalStyling from '../../utils/styles';
-
-const iconNames = [
-  'build-outline',
-  'construct-outline',
-  'settings-outline',
-  'hammer-outline',
-  'reader-outline',
-  'add-circle-outline',
-  'alert-circle-outline',
-  'bag-add-outline',
-  'battery-half',
-  'battery-full',
-  'briefcase-outline',
-  'brush-outline',
-  'bulb-outline',
-  'calculator-outline',
-  'calendar-number-outline',
-  'calendar-outline',
-  'call-outline',
-  'camera-outline',
-  'car-outline',
-  'car-sport-outline',
-  'card-outline',
-  'cart-outline',
-  'cash-outline',
-  'cellular-outline',
-  'chatbox-ellipses-outline',
-  'chatbubble-ellipses-outline',
-  'checkbox-outline',
-  'checkmark-circle-outline',
-  'checkmark-done',
-  'checkmark-outline',
-  'clipboard-outline',
-  'color-fill-outline',
-  'cut-outline',
-  'desktop-outline',
-  'document-outline',
-  'document-text-outline',
-  'extension-puzzle-outline',
-  'flame-outline',
-  'flash-outline',
-  'flashlight-outline',
-  'flask-outline',
-  'funnel-outline',
-  'help',
-  'home-outline',
-  'hourglass-outline',
-  'information',
-  'key-outline',
-  'library-outline',
-  'list-outline',
-  'location-outline',
-  'lock-closed-outline',
-  'lock-open-outline',
-  'wifi-outline',
-  'mail-outline',
-  'man-outline',
-  'pencil-outline',
-  'people-outline',
-  'power-outline',
-  'print-outline',
-  'save-outline',
-  'videocam-outline',
-];
-
-const taskInit: ITask = {
-  name: '',
-  clientPhoneNumber: '',
-  phases: [],
-  status: taskStatus.Pending,
-};
+import {useAddTask} from './utils';
+import iconNames from '../../constants/iconNames';
 
 const AddTask = () => {
-  const [showTasPhaseContainer, setShowTaskPhaseContainer] = useState(false);
-  const [task, setTask] = useState<ITask>(taskInit);
-  const [name, setName] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const [showPickerPopup, setShowPickerPopup] = useState(false);
-  const [iconName, setIconName] = useState<string>('');
+  // Hooks
+  const {
+    showTasPhaseContainer,
+    name,
+    description,
+    iconName,
+    task,
+    showPickerPopup,
+    setName,
+    setDescription,
+    addPhase,
+    setShowPickerPopup,
+    showTaskForm,
+    displayPreviousPhase,
+    updateTaskFormDetails,
+    validateTaskDetails,
+    setSelectIcon,
+  } = useAddTask();
 
   const styles = getStyling();
   const globalStyles = getGlobalStyling();
-
-  const validateTaskDetails = () => {
-    if (!task.name && !task.clientPhoneNumber) {
-      console.log('Please fill in the task details');
-    } else if (!task.name) {
-      console.log('Please fill in the task name');
-    } else if (!task.clientPhoneNumber) {
-      console.log('Please fill in the client phone number');
-    } else {
-      setShowTaskPhaseContainer(true);
-    }
-  };
-
-  console.log('task::', task);
-
-  const updateTaskFormDetails = (value: string, field: TaskFormFieldEnum) => {
-    switch (field) {
-      case TaskFormFieldEnum.name:
-        setTask({...task, name: value});
-        break;
-      case TaskFormFieldEnum.phoneNumber:
-        setTask({...task, clientPhoneNumber: value});
-        break;
-      case TaskFormFieldEnum.description:
-        setTask({...task, description: value});
-        break;
-      default:
-        setTask(task);
-    }
-  };
-
-  const showTaskForm = (value: boolean) => {
-    setName('');
-    setDescription('');
-    setShowTaskPhaseContainer(value);
-  };
-
-  const displayPreviousPhase = () => {
-    let poppedPhase = task.phases.pop();
-
-    if (poppedPhase) {
-      setName(poppedPhase.name);
-      setDescription(poppedPhase.description ?? '');
-    }
-  };
-
-  const addPhase = () => {
-    if (!name && !description) {
-    } else if (!name) {
-    } else if (!description) {
-    } else {
-      let taskNumber = task.phases.length + 1;
-      let taskPhase = task.phases;
-
-      taskPhase.push({
-        name,
-        description,
-        status: taskPhaseStatus.Pending,
-        number: taskNumber,
-        icon: iconName,
-      });
-
-      setTask({
-        ...task,
-        phases: taskPhase,
-      });
-      setName('');
-      setDescription('');
-      setIconName('');
-    }
-  };
-
-  const setSelectIcon = (name: string) => {
-    setIconName(name);
-    setShowPickerPopup(false);
-  };
 
   return (
     <View style={styles.container}>
--- src/screens/addTask/utils.ts
+++ src/screens/addTask/utils.ts
@@ -1,3 +1,114 @@
+import {useState} from 'react';
+import {ITask} from '../../interfaces';
+import {TaskFormFieldEnum, taskStatus} from '../../utils/enums';
+
+const taskInit: ITask = {
+  name: '',
+  clientPhoneNumber: '',
+  phases: [],
+  status: taskStatus.Pending,
+};
+
 export const useAddTask = () => {
-  return {};
+  // Hooks
+  const [showTasPhaseContainer, setShowTaskPhaseContainer] = useState(false);
+  const [task, setTask] = useState<ITask>(taskInit);
+  const [name, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [showPickerPopup, setShowPickerPopup] = useState(false);
+  const [iconName, setIconName] = useState<string>('');
+
+  // Methods
+  const validateTaskDetails = () => {
+    if (!task.name && !task.clientPhoneNumber) {
+      console.log('Please fill in the task details');
+    } else if (!task.name) {
+      console.log('Please fill in the task name');
+    } else if (!task.clientPhoneNumber) {
+      console.log('Please fill in the client phone number');
+    } else {
+      setShowTaskPhaseContainer(true);
+    }
+  };
+  console.log('task::', task);
+
+  const updateTaskFormDetails = (value: string, field: TaskFormFieldEnum) => {
+    switch (field) {
+      case TaskFormFieldEnum.name:
+        setTask({...task, name: value});
+        break;
+      case TaskFormFieldEnum.phoneNumber:
+        setTask({...task, clientPhoneNumber: value});
+        break;
+      case TaskFormFieldEnum.description:
+        setTask({...task, description: value});
+        break;
+      default:
+        setTask(task);
+    }
+  };
+
+  const showTaskForm = (value: boolean) => {
+    setName('');
+    setDescription('');
+    setShowTaskPhaseContainer(value);
+  };
+
+  const displayPreviousPhase = () => {
+    let poppedPhase = task.phases.pop();
+
+    if (poppedPhase) {
+      setName(poppedPhase.name);
+      setDescription(poppedPhase.description ?? '');
+    }
+  };
+
+  const addPhase = () => {
+    if (!name && !description) {
+    } else if (!name) {
+    } else if (!description) {
+    } else {
+      let taskNumber = task.phases.length + 1;
+      let taskPhase = task.phases;
+
+      taskPhase.push({
+        name,
+        description,
+        status: taskPhaseStatus.Pending,
+        number: taskNumber,
+        icon: iconName,
+      });
+
+      setTask({
+        ...task,
+        phases: taskPhase,
+      });
+      setName('');
+      setDescription('');
+      setIconName('');
+    }
+  };
+
+  const setSelectIcon = (name: string) => {
+    setIconName(name);
+    setShowPickerPopup(false);
+  };
+
+  return {
+    showTasPhaseContainer,
+    name,
+    description,
+    iconName,
+    task,
+    showPickerPopup,
+    setName,
+    setDescription,
+    addPhase,
+    setShowPickerPopup,
+    showTaskForm,
+    displayPreviousPhase,
+    updateTaskFormDetails,
+    validateTaskDetails,
+    setSelectIcon,
+  };
 };
